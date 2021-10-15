305c6ed90458c4c97cb79b417807005ffffb303a
Davidxing1 <937104001@qq.com>
2021-10-15 18:10:43 +0800

    test 更新了组件

---
+++ src/pages/contributors/index.tsx
@@ -0,0 +1,12 @@
+import Team from "../../components/team";
+
+
+const Contributors = () =>{
+    return (
+        <div>
+            <Team></Team>
+        </div>
+    )
+}
+
+export default  Contributors
